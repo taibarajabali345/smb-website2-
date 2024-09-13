6afff7b8bd9b66fa837ae549fb8aad227359719d
taibarajabali345 <156583263+taibarajabali345@users.noreply.github.com>
2024-09-13 13:23:40 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const links = document.querySelectorAll('nav ul li a');
+//     links.forEach(link => {
+//         link.addEventListener('click', function() {
+//             alert(`You clicked on ${link.textContent}`);
+//         });
+//     });
+// });
